6d65809f6c5051030310803250e9b9ed46e45d44
Медербек Шермаматов <msmeda0@gmail.com>
2022-10-10 21:49:29 +0600

    Preparing core Meder

---
+++ app/src/main/java/kg/o/internlabs/omarket/presentation/mainactivity/MainActivity.kt
@@ -0,0 +1,19 @@
+package kg.o.internlabs.omarket.presentation.mainactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import kg.o.internlabs.omarket.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var model: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        model = ViewModelProvider(this)[MainViewModel::class.java]
+    }
+}
---
+++ app/src/main/java/kg/o/internlabs/omarket/presentation/mainactivity/MainViewModel.kt
@@ -0,0 +1,6 @@
+package kg.o.internlabs.omarket.presentation.mainactivity
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
