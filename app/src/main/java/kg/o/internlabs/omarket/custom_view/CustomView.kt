593a46e788d76fd991c932d4c7fd78f10dd78749
Yana Tikhnenko <tikhnenko97yana@gmail.com>
2022-10-20 21:27:31 +0600

    otp_custom_view

---
+++ app/src/main/java/kg/o/internlabs/omarket/custom_view/CustomView.kt
@@ -0,0 +1,102 @@
+package kg.o.internlabs.omarket.custom_view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kg.o.internlabs.omarket.R
+
+class CustomView : ConstraintLayout {
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    private var rvOTP: RecyclerView? = null
+
+    private val adapter by lazy { RVAdapter() }
+
+
+    init {
+        val view = View.inflate(context, R.layout.custom_view, this)
+        rvOTP = view.findViewById(R.id.rvOTP)
+
+        val manager = GridLayoutManager(context, 6)
+        rvOTP?.layoutManager = manager
+        rvOTP?.adapter = adapter
+        val list = arrayListOf<OptEntity>()
+        for (i in 0..5){
+            list.add(OptEntity("", false))
+        }
+
+//        list.map { it.error = true }
+
+        adapter.submitList(list)
+    }
+
+    fun updateTv(text: String){
+        text.split("")
+        if (text.length == 6){
+
+        }
+    }
+
+
+}
+
+data class OptEntity(
+    val text: String,
+    var error: Boolean
+)
+
+class RVAdapter : ListAdapter<OptEntity, RVViewHolder>(otpDiff) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVViewHolder {
+        return RVViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: RVViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class RVViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+    private var tvTitle: TextView? = null
+    init {
+        tvTitle = view.findViewById(R.id.one)
+    }
+
+    fun bind(item: OptEntity?) {
+        tvTitle?.text = item?.text
+    }
+
+    companion object{
+        fun create(parent: ViewGroup): RVViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_custom, parent, false)
+            return RVViewHolder(view)
+        }
+    }
+
+}
+
+val otpDiff = object : DiffUtil.ItemCallback<OptEntity>() {
+    override fun areItemsTheSame(oldItem: OptEntity, newItem: OptEntity): Boolean {
+       return  oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: OptEntity, newItem: OptEntity): Boolean {
+        return  oldItem.error == newItem.error &&  oldItem.text == newItem.text
+    }
+
+}
