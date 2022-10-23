cdafac951af47ab87466f451379a23238f911152
Yana Tikhnenko <tikhnenko97yana@gmail.com>
2022-10-23 14:21:51 +0600

    otp_custom_view

--- core/src/main/java/kg/o/internlabs/core/custom_view/OtpInputCustomView.kt
+++ core/src/main/java/kg/o/internlabs/core/custom_view/OtpInputCustomView.kt
@@ -18,38 +18,27 @@ import kg.o.internlabs.core.databinding.OtpInputCustomViewBinding.inflate
 
 class OtpInputCustomView : ConstraintLayout {
 
-    private val binding: OtpInputCustomViewBinding =
-        inflate(LayoutInflater.from(context), this, true)
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    private val binding: OtpInputCustomViewBinding = inflate(LayoutInflater.from(context), this, true)
 
     private val adapter by lazy { RVAdapter() }
 
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
-        context.obtainStyledAttributes(attrs, R.styleable.OtpInputCustomView).run {
-            getText(R.styleable.OtpInputCustomView_otp_custom_view)?.let {
-                otpAttrs(it.toString().toInt())
-            }
-            recycle()
-        }
-    }
 
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    init {
 
-
-//    fun updateTv(text: String) {
-//        text.split("")
-//        if (text.length == 6) {
-//
-//        }
-//    }
-
-    private fun otpAttrs(input: Int) {
-        val manager = GridLayoutManager(context, input)
+        val manager = GridLayoutManager(context, 6)
         binding.rvOTP.layoutManager = manager
         binding.rvOTP.adapter = adapter
         val list = arrayListOf<OptEntity>()
-        for (i in 0 until input) {
-            list.add(OptEntity())
+        for (i in 0..5){
+            list.add(OptEntity("", false))
         }
 
 //        list.map { it.error = true }
@@ -57,12 +46,19 @@ class OtpInputCustomView : ConstraintLayout {
         adapter.submitList(list)
     }
 
+    fun updateTv(text: String){
+        text.split("")
+        if (text.length == 6){
+
+        }
+    }
+
 
 }
 
 data class OptEntity(
-    val text: String = "",
-    var error: Boolean = false
+    val text: String,
+    var error: Boolean
 )
 
 class RVAdapter : ListAdapter<OptEntity, RVViewHolder>(otpDiff) {
@@ -78,7 +74,6 @@ class RVAdapter : ListAdapter<OptEntity, RVViewHolder>(otpDiff) {
 class RVViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
     private var tvTitle: TextView? = null
-
     init {
         tvTitle = view.findViewById(R.id.one)
     }
@@ -87,10 +82,9 @@ class RVViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         tvTitle?.text = item?.text
     }
 
-    companion object {
+    companion object{
         fun create(parent: ViewGroup): RVViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.item_recycler_custom, parent, false)
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_custom, parent, false)
             return RVViewHolder(view)
         }
     }
@@ -99,11 +93,11 @@ class RVViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
 val otpDiff = object : DiffUtil.ItemCallback<OptEntity>() {
     override fun areItemsTheSame(oldItem: OptEntity, newItem: OptEntity): Boolean {
-        return oldItem == newItem
+       return  oldItem == newItem
     }
 
     override fun areContentsTheSame(oldItem: OptEntity, newItem: OptEntity): Boolean {
-        return oldItem.error == newItem.error && oldItem.text == newItem.text
+        return  oldItem.error == newItem.error &&  oldItem.text == newItem.text
     }
 
 }
