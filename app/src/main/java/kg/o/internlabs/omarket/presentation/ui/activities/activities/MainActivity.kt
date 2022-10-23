0fa7d4430344ed1e457862f14e27f4aa4ab54e1b
Рамис Абасов <ramis040404@gmail.com>
2022-10-23 19:34:29 +0600

    Add Retrofit,OkHttp, ApiService interface

--- app/src/main/java/kg/o/internlabs/omarket/presentation/ui/activities/activities/MainActivity.kt
+++ app/src/main/java/kg/o/internlabs/omarket/presentation/ui/activities/activities/MainActivity.kt
@@ -1,11 +1,16 @@
 package kg.o.internlabs.omarket.presentation.ui.activities.activities
 
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import kg.o.internlabs.core.base.BaseActivity
 import kg.o.internlabs.core.model.Register
+import kg.o.internlabs.core.network.RetrofitClient
 import kg.o.internlabs.omarket.databinding.ActivityMainBinding
 import kg.o.internlabs.omarket.presentation.viewmodels.activitiesviewmodels.MainViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
@@ -22,6 +27,22 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
     override fun initView() {
         super.initView()
         //binding.navHost
+        binding.btnGet.setOnClickListener{
+            RetrofitClient.instance.registerUser("ypsGjpmpDR9GndA9YvzBHpIqXsFStfyq",register).enqueue(object :
+                Callback<Register> {
+                override fun onResponse(call: Call<Register>, response: Response<Register>) {
+                    if (response.isSuccessful) {
+                        println("call = " + call + ", response = " + response.body())
+                        Toast.makeText(this@MainActivity, "Yo", Toast.LENGTH_SHORT).show()
+                    }
+                    Toast.makeText(this@MainActivity, "response fail ${response.message()}", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onFailure(call: Call<Register>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, "failure", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
     }
 
 
