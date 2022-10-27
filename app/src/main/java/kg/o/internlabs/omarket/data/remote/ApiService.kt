b6d29f9bfc6ec048744313642383f9d8fdef5f83
Рамис Абасов <ramis040404@gmail.com>
2022-10-27 17:51:56 +0600

    Update final Retrofit OKHttp tasks

--- app/src/main/java/kg/o/internlabs/omarket/data/remote/ApiService.kt
+++ app/src/main/java/kg/o/internlabs/omarket/data/remote/ApiService.kt
@@ -9,25 +9,21 @@ import retrofit2.http.POST
 interface ApiService {
     @POST("api/market-auth/register/")
     fun registerUser(
-        @Header("API_KEY") apiKey: String,
         @Body reg: Register?
     ): Call<RegResponse>
 
     @POST("api/market-auth/check-otp/")
     fun checkOtp(
-        @Header("API_KEY") apiKey: String,
         @Body otp: Otp?
     ): Call<Token>
 
     @POST("api/market-auth/refresh-token/")
     fun refreshToken(
-        @Header("API_KEY") apiKey: String,
         @Body reg: RefreshToken?
     ): Call<Token>
 
     @POST("api/market-auth/auth/msisdn-password/")
     fun loginUser(
-        @Header("API_KEY") apiKey: String,
         @Body reg: Login?
     ): Call<Token>
 }
