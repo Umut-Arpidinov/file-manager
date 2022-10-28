c576a26a6d8b2a02dc1e53b7a3d7a562d287ae7e
Медербек Шермаматов <msmeda0@gmail.com>
2022-10-28 21:38:45 +0600

    test

--- core/src/main/java/kg/o/internlabs/core/cells/CustomOneTitleTextCells.kt
+++ core/src/main/java/kg/o/internlabs/core/cells/CustomOneTitleTextCells.kt
@@ -2,10 +2,16 @@ package kg.o.internlabs.core.cells
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import kg.o.internlabs.core.R
+import kg.o.internlabs.core.databinding.OneTitleTextCellBinding
 
 class CustomOneTitleTextCells : View {
+    private val binding: OneTitleTextCellBinding = inflate(LayoutInflater
+        .from(context), this, true)
+
     private var position= "single"
     private var hasIcon = false
     private var hasDetails = false
@@ -16,7 +22,7 @@ class CustomOneTitleTextCells : View {
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         context.obtainStyledAttributes(attrs, R.styleable.OtpInputCustomView).run {
             getText(R.styleable.OtpInputCustomView_otp_custom_view)?.let {
-                setNumber(it.toString().toInt())
+                //setNumber(it.toString().toInt())
             }
             recycle()
         }
@@ -28,3 +34,70 @@ class CustomOneTitleTextCells : View {
         defStyleAttr
     )
 }
+
+/*
+class NumberInputView : ConstraintLayout {
+    private val binding: CustomInputFieldBinding = inflate(
+        LayoutInflater.from(context), this, true
+    )
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        context.obtainStyledAttributes(attributeSet, R.styleable.NumberInputView).run {
+            getText(R.styleable.NumberInputView_helperTextState)?.let {
+                setHintText(it.toString())
+            }
+            recycle()
+        }
+
+    }
+
+
+    private fun setHintText(state: String) = with(binding) {
+        val numberNotFound = context.getString(R.string.number_mistake)
+        val enterNumber = context.getString(R.string.enter_number)
+
+        when (state) {
+            numberNotFound -> {
+                cancelImageLogic()
+                with(numberInputHelperText) {
+                    text = numberNotFound
+                    setTextColor(ContextCompat.getColor(context, R.color.red_1))
+                }
+                numberInputCancelImage.visibility = View.VISIBLE
+                numberInputFrame.background = ResourcesCompat.getDrawable(
+                    resources, R.drawable.number_not_ok_style, null
+                )
+                enterNumberEditText.background = ResourcesCompat.getDrawable(
+                    resources, R.drawable.number_not_ok_style, null
+                )
+
+            }
+            enterNumber -> {
+                println("Enter number happened ")
+                numberInputHelperText.text = enterNumber
+                numberInputCancelImage.visibility = View.INVISIBLE
+            }
+
+        }
+    }
+
+    private fun cancelImageLogic() = with(binding){
+        val enterNumber = context.getString(R.string.enter_number)
+        numberInputCancelImage.setOnClickListener {
+            numberInputHelperText.text = enterNumber
+            numberInputCancelImage.visibility = View.GONE
+            numberInputFrame.background = ResourcesCompat.getDrawable(
+                resources, R.drawable.number_ok_style, null
+            )
+            enterNumberEditText.background = ResourcesCompat.getDrawable(
+                resources, R.drawable.number_ok_style, null)
+            numberInputHelperText.setTextColor(ContextCompat.getColor(context, R.color.black_1))
+            enterNumberEditText.text = null
+
+        }
+    }
+
+
+}*/
