1ee0619e20c2b38dcea6a6e0105ebc303d771bcb
Рамис Абасов <ramis040404@gmail.com>
2022-10-29 15:48:51 +0600

    Change model, create RetrofitInstance

--- core/src/main/java/kg/o/internlabs/core/data/remote/network/RetrofitClient.kt
+++ core/src/main/java/kg/o/internlabs/core/data/remote/network/RetrofitClient.kt
@@ -1,14 +1,14 @@
 package kg.o.internlabs.core.data.remote.network
 
-import kg.o.internlabs.core.BuildConfig.API_URL
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
+
 object RetrofitClient {
+    const val API_KEY = "ypsGjpmpDR9GndA9YvzBHpIqXsFStfyq"
+
 
     private fun interceptor(): Interceptor {
         val interceptor = HttpLoggingInterceptor()
@@ -19,7 +19,7 @@ object RetrofitClient {
     private fun authInterceptor(): Interceptor {
         val authInterceptor = Interceptor { chain ->
             val request = chain.request().newBuilder().apply {
-                addHeader("Content-type", "application/json").addHeader("API_KEY", "ypsGjpmpDR9GndA9YvzBHpIqXsFStfyq")
+                addHeader("Content-type", "application/json").addHeader("API_KEY", API_KEY)
             }.build()
             chain.proceed(request)
         }
@@ -32,8 +32,4 @@ object RetrofitClient {
         .connectTimeout(20, TimeUnit.SECONDS)
         .addInterceptor(interceptor()).addInterceptor(authInterceptor())
         .build()
-
-
-
-
 }
