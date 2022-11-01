644510b736ababc8baf3e2158a1d6c61efc5d96d
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2022-11-01 18:01:50 +0300

    Очистил неиспользоваемые методы и его лэйауты

--- core/src/main/java/kg/o/internlabs/core/CustomToast.kt
+++ core/src/main/java/kg/o/internlabs/core/CustomToast.kt
@@ -2,24 +2,19 @@ package kg.o.internlabs.core
 
 import android.app.Activity
 import android.graphics.Color
-import android.os.CountDownTimer
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
-//Activity -> MainActivity
-
-class CustomToasts(private val mainActivity: Activity) {
+class CustomToasts(private val activity: Activity) {
 
     fun snackButtonLoading(text: String, layout: View) {
         val snackBar = Snackbar.make(layout, "", Snackbar.LENGTH_INDEFINITE)
         snackBar.view.setBackgroundColor(Color.TRANSPARENT)
         val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
 
-        val customView = mainActivity.layoutInflater.inflate(
-            R.layout.toast_loading, mainActivity.findViewById(R.id.loading_wrapper)
+        val customView = activity.layoutInflater.inflate(
+            R.layout.toast_loading, activity.findViewById(R.id.loading_wrapper)
         )
 
         val textMessage: TextView = customView.findViewById(R.id.toastLoadingMessage)
@@ -39,8 +34,8 @@ class CustomToasts(private val mainActivity: Activity) {
         snackBar.view.setBackgroundColor(Color.TRANSPARENT)
         val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
 
-        val customView = mainActivity.layoutInflater.inflate(
-            R.layout.toast_success, mainActivity.findViewById(R.id.success_wrapper)
+        val customView = activity.layoutInflater.inflate(
+            R.layout.toast_success, activity.findViewById(R.id.success_wrapper)
         )
 
         val textMessage: TextView = customView.findViewById(R.id.toastSuccessMessage)
@@ -55,8 +50,8 @@ class CustomToasts(private val mainActivity: Activity) {
         snackBar.view.setBackgroundColor(Color.TRANSPARENT)
         val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
 
-        val customView = mainActivity.layoutInflater.inflate(
-            R.layout.toast_error, mainActivity.findViewById(R.id.error_wrapper)
+        val customView = activity.layoutInflater.inflate(
+            R.layout.toast_error, activity.findViewById(R.id.error_wrapper)
         )
 
         val textMessage: TextView = customView.findViewById(R.id.toastErrorMessage)
@@ -66,53 +61,13 @@ class CustomToasts(private val mainActivity: Activity) {
         snackBar.show()
     }
 
-    fun snackButtonTimer(text: String, btnText: String, layout: View, seconds: Long) {
-
-        val snackBar = Snackbar.make(layout, "", Snackbar.LENGTH_INDEFINITE)
-        snackBar.view.setBackgroundColor(Color.TRANSPARENT)
-        val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
-
-        val customView = mainActivity.layoutInflater.inflate(
-            R.layout.toast_timer, mainActivity.findViewById(R.id.timer_wrapper)
-        )
-
-        val textMessage: TextView = customView.findViewById(R.id.toastTimerMessage)
-        val timerText: TextView = customView.findViewById(R.id.toastTimer)
-        val button: Button = customView.findViewById(R.id.toastTimerButton)
-
-        object : CountDownTimer(seconds * 1000, 1000) {
-            override fun onTick(p0: Long) {
-                timerText.text = "$p0"
-            }
-
-            override fun onFinish() {
-                timerText.text = "0"
-                snackBarLayout.removeAllViews()
-            }
-        }.start()
-
-        button.setOnClickListener {
-            snackBarLayout.removeAllViews()
-            Toast.makeText(mainActivity, "Logic to be done.", Toast.LENGTH_SHORT).show()
-            snackBar.dismiss()
-        }
-
-        textMessage.text = text
-        button.text = btnText
-
-        snackBarLayout.addView(customView, 0)
-        snackBar.show()
-
-        TODO("Не используемый snackBar в приложении, но нужно пофиксить детали позже")
-    }
-
     fun snackButtonSystem(text: String, layout: View) {
         val snackBar = Snackbar.make(layout, "", Snackbar.LENGTH_LONG)
         snackBar.view.setBackgroundColor(Color.TRANSPARENT)
         val snackBarLayout = snackBar.view as Snackbar.SnackbarLayout
 
-        val customView = mainActivity.layoutInflater.inflate(
-            R.layout.toast_system, mainActivity.findViewById(R.id.system_wrapper)
+        val customView = activity.layoutInflater.inflate(
+            R.layout.toast_system, activity.findViewById(R.id.system_wrapper)
         )
 
         val textMessage: TextView = customView.findViewById(R.id.textSystem)
