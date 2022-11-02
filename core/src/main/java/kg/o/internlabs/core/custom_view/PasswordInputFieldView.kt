64e9d75c36502b5e226e4641b35b9902d1e18b36
Умут Арпидинов <ymytbek0314@gmail.com>
2022-11-03 00:52:27 +0600

    update branch

--- core/src/main/java/kg/o/internlabs/core/custom_view/PasswordInputFieldView.kt
+++ core/src/main/java/kg/o/internlabs/core/custom_view/PasswordInputFieldView.kt
@@ -3,23 +3,59 @@ package kg.o.internlabs.core.custom_view
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import kg.o.internlabs.core.R
-import kg.o.internlabs.core.databinding.CustomPasswordInputFieldViewBinding
-import kg.o.internlabs.core.databinding.CustomPasswordInputFieldViewBinding.inflate
+import kg.o.internlabs.core.databinding.PasswordInputViewBinding
+import kg.o.internlabs.core.databinding.PasswordInputViewBinding.inflate
 
-class PasswordInputFieldView : ConstraintLayout{
-    private val binding: CustomPasswordInputFieldViewBinding= inflate(LayoutInflater.from(context),this,true)
+
+class PasswordInputFieldView : ConstraintLayout {
+    private val binding: PasswordInputViewBinding =
+        inflate(LayoutInflater.from(context), this, true)
+
     constructor(context: Context) : super(context)
-    constructor(context: Context,attrs: AttributeSet?) : super(context,attrs){
-        context.obtainStyledAttributes(attrs, R.styleable.PasswordInputFieldView).run{
-            getText(R.styleable.NumberInputView_helperTextState)?.let {
-
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        context.obtainStyledAttributes(attrs, R.styleable.PasswordInputFieldView).run {
+            getText(R.styleable.PasswordInputFieldView_helperTextSate)?.let {
+                setHelperText(it.toString())
             }
+            getText(R.styleable.PasswordInputFieldView_passwordHint)?.let {
+                setPasswordHint(it.toString())
+            }
+            recycle()
 
         }
     }
-    private fun setHintText(state: String) = with(binding){
+
+    fun setHelperText(state: String) = with(binding) {
         passwordHelper.text = state
+        if(state == context.getString(R.string.incorrect_password)|| state == context.getString(R.string.password_not_match)){
+            passwordToggle.visibility = View.VISIBLE
+            setFrameErrorColor()
+            setTextErrorColor()
+            passwordToggle.setOnClickListener{
+
+            }
+        }
+
+
+
+    }
+
+    fun setPasswordHint(text: String) = with(binding) {
+        passwordInputField.hint = text
+    }
+
+    private fun setFrameErrorColor() = with(binding) {
+        frame.background = ResourcesCompat.getDrawable(
+            resources, R.drawable.number_not_ok_style, null
+        )
+    }
+
+    private fun setTextErrorColor() = with(binding) {
+        passwordHelper.setTextColor(ContextCompat.getColor(context, R.color.red_1))
     }
 }
