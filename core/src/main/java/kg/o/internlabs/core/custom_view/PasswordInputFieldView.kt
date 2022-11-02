79ced63149f07ec21b4154e4406d029c04e50e72
Умут Арпидинов <ymytbek0314@gmail.com>
2022-11-02 22:24:16 +0600

    add  attr

--- core/src/main/java/kg/o/internlabs/core/custom_view/PasswordInputFieldView.kt
+++ core/src/main/java/kg/o/internlabs/core/custom_view/PasswordInputFieldView.kt
@@ -1,6 +1,25 @@
 package kg.o.internlabs.core.custom_view
 
-class PasswordInputFieldView {
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import kg.o.internlabs.core.R
+import kg.o.internlabs.core.databinding.CustomPasswordInputFieldViewBinding
+import kg.o.internlabs.core.databinding.CustomPasswordInputFieldViewBinding.inflate
 
+class PasswordInputFieldView : ConstraintLayout{
+    private val binding: CustomPasswordInputFieldViewBinding= inflate(LayoutInflater.from(context),this,true)
+    constructor(context: Context) : super(context)
+    constructor(context: Context,attrs: AttributeSet?) : super(context,attrs){
+        context.obtainStyledAttributes(attrs, R.styleable.PasswordInputFieldView).run{
+            getText(R.styleable.NumberInputView_helperTextState)?.let {
 
+            }
+
+        }
+    }
+    private fun setHintText(state: String) = with(binding){
+        passwordHelper.text = state
+    }
 }
