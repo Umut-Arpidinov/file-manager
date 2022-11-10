323c0e8ff70c7b9438d7bf104ece781129862b23
Yana Tikhnenko <tikhnenko97yana@gmail.com>
2022-11-10 14:28:59 +0600

    splash_screen_error

--- app/src/main/java/kg/o/internlabs/omarket/presentation/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/kg/o/internlabs/omarket/presentation/ui/activities/SplashScreenActivity.kt
@@ -2,10 +2,12 @@ package kg.o.internlabs.omarket.presentation.ui.activities
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.Window
+import android.view.WindowInsets
 import android.view.WindowManager
 import dagger.hilt.android.AndroidEntryPoint
 import kg.o.internlabs.core.base.BaseActivity
@@ -32,29 +34,21 @@ class SplashScreenActivity : BaseActivity<BaseViewModel, ActivitySplashScreenBin
     override fun initView() {
         super.initView()
         Handler(Looper.getMainLooper()).postDelayed({
-
-            // Start activity
             startActivity(Intent(this, MainActivity::class.java))
-
-            // Animate the loading of new activity
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-
-            // Close this activity
             finish()
-
         }, 4000)
     }
 
+    @Suppress("DEPRECATION")
     private fun makeFullScreen() {
-        // Remove Title
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-
-        // Make Fullscreen
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-        // Hide the toolbar
-        supportActionBar?.hide()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
