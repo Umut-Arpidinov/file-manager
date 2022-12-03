b530cdaebe24619febdc44a977e2c7e80c74b83e
Медербек Шермаматов <msmeda0@gmail.com>
2022-12-03 18:47:34 +0600

    optimized some moments  Meder

--- app/src/main/java/kg/o/internlabs/omarket/data/MapperForModels.kt
+++ app/src/main/java/kg/o/internlabs/omarket/data/MapperForModels.kt
@@ -6,37 +6,31 @@ import retrofit2.Response
 
 class MapperForModels {
 
-    fun mapEntityToDbModel(reg: RegisterEntity?): RegisterDto {
-        return RegisterDto(
-            accessToken = reg?.accessToken,
-            refreshToken = reg?.refreshToken,
-            message = reg?.message,
-            msisdn = reg?.msisdn,
-            otp = reg?.otp,
-            success = reg?.success,
-            password = reg?.password,
-            password2 = reg?.password2
-        )
-    }
+    fun mapEntityToDbModel(reg: RegisterEntity?) = RegisterDto(
+        accessToken = reg?.accessToken,
+        refreshToken = reg?.refreshToken,
+        message = reg?.message,
+        msisdn = reg?.msisdn,
+        otp = reg?.otp,
+        success = reg?.success,
+        password = reg?.password,
+        password2 = reg?.password2
+    )
 
-    private fun mapDbModelToEntity(regDto: RegisterDto?): RegisterEntity {
-        return RegisterEntity(
-            accessToken = regDto?.accessToken,
-            refreshToken = regDto?.refreshToken,
-            message = regDto?.message,
-            msisdn = regDto?.msisdn,
-            otp = regDto?.otp,
-            success = regDto?.success,
-            password = regDto?.password,
-            password2 = regDto?.password2
-        )
-    }
+    private fun mapDbModelToEntity(regDto: RegisterDto?) = RegisterEntity(
+        accessToken = regDto?.accessToken,
+        refreshToken = regDto?.refreshToken,
+        message = regDto?.message,
+        msisdn = regDto?.msisdn,
+        otp = regDto?.otp,
+        success = regDto?.success,
+        password = regDto?.password,
+        password2 = regDto?.password2
+    )
 
-    fun mapRespDbModelToRespEntity(list: Response<RegisterDto?>): Response<RegisterEntity>? {
-        return if (list.isSuccessful) {
-            Response.success(mapDbModelToEntity(list.body()))
-        } else {
-            list.errorBody()?.let { Response.error(list.code(), it) }
-        }
+    fun mapRespDbModelToRespEntity(list: Response<RegisterDto?>) = if (list.isSuccessful) {
+        Response.success(mapDbModelToEntity(list.body()))
+    } else {
+        list.errorBody()?.let { Response.error(list.code(), it) }
     }
 }
