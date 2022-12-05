e49c1724f94e024fa1774138c58189decf0e85c5
Bektur <yasultanovbektur@yandex.ru>
2022-12-05 15:31:14 +0600

    Change parameters of DotPagerAdapter: Bek

--- app/src/main/java/kg/o/internlabs/omarket/presentation/ui/fragments/main/PagerImageAdapter.kt
+++ app/src/main/java/kg/o/internlabs/omarket/presentation/ui/fragments/main/PagerImageAdapter.kt
@@ -1,5 +1,6 @@
 package kg.o.internlabs.omarket.presentation.ui.fragments.main
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -8,17 +9,13 @@ import kg.o.internlabs.omarket.databinding.PagerItemImageMainBinding
 //#TODO(Make a RecyclerAdapter for main page and complete adapter that depends on INPUT)
 
 internal class PagerImageAdapter internal constructor(
-    private var count: Int,
+    private val context: Context,
+    private val imageURLs: Array<String>,
     private val itemWidth: Int
 ) :
     RecyclerView.Adapter<PagerImageAdapter.ViewHolder>() {
 
     internal class ViewHolder(val binding: PagerItemImageMainBinding) : RecyclerView.ViewHolder(binding.root)
-
-    fun setCount(count: Int) {
-        this.count = count
-        notifyDataSetChanged()
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = PagerItemImageMainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -30,11 +27,11 @@ internal class PagerImageAdapter internal constructor(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(holder){
-//            binding.itemImgMain.setImageResource(kg.o.internlabs.core.R.drawable.img_sample)
+
         }
     }
 
     override fun getItemCount(): Int {
-        return count
+        return imageURLs.size
     }
 }
