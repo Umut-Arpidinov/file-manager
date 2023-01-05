f516b36cedbd220d3c716a46288d8fb609f70997
Умут Арпидинов <ymytbek0314@gmail.com>
2023-01-05 11:48:20 +0600

    refactor

--- app/src/main/java/kg/o/internlabs/omarket/domain/entity/ads/LocationX.kt
+++ app/src/main/java/kg/o/internlabs/omarket/domain/entity/ads/LocationX.kt
@@ -4,6 +4,10 @@ data class LocationX(
     var id: Int? = null,
     var name: String? = null,
     var locationType: String? = null,
+    val orderNum: Int? = null,
+    val isPopular: Boolean? = null,
     var parent: Int? = null,
+    val searchByName: String? = null,
     var subLocationsId: List<Int>? = null
+
 )
