39cf7841e6bd4d575f1022f72a83c3a3cf6f35a0
Умут Арпидинов <ymytbek0314@gmail.com>
2023-01-05 12:04:14 +0600

    delete unused statements

--- app/src/main/java/kg/o/internlabs/omarket/domain/entity/ads/LocationX.kt
+++ app/src/main/java/kg/o/internlabs/omarket/domain/entity/ads/LocationX.kt
@@ -4,9 +4,9 @@ data class LocationX(
     var id: Int? = null,
     var name: String? = null,
     var locationType: String? = null,
+    var parent: Int? = null,
     val orderNum: Int? = null,
     val isPopular: Boolean? = null,
-    var parent: Int? = null,
     val searchByName: String? = null,
     var subLocationsId: List<Int>? = null
 
