04393ba011b2c2c38185e5de2a386561268e7194
asnur <mullmeru@gmail.com>
2023-01-18 00:11:34 +0600

    fix(permissions): fixed permissions requester for higher versions of android

--- app/src/main/java/kg/o/internlabs/omarket/presentation/ui/fragments/new_ads/NewAdsFragment.kt
+++ app/src/main/java/kg/o/internlabs/omarket/presentation/ui/fragments/new_ads/NewAdsFragment.kt
@@ -258,7 +258,6 @@ class NewAdsFragment : BaseFragment<FragmentNewAdsBinding, NewAdsViewModel>(),
     }
 
     private fun pickImages() {
-        checkPermission()
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         intent.addCategory(Intent.CATEGORY_OPENABLE)
