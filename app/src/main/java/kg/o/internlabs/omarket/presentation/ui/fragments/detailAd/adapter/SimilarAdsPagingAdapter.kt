5017db123dd7b7d28a07cfb920ed7f48d9036dc3
Bektur <yasultanovbektur@yandex.ru>
2023-01-17 12:03:11 +0600

    Handle exception with old price for other adapters: Bek

--- app/src/main/java/kg/o/internlabs/omarket/presentation/ui/fragments/detailAd/adapter/SimilarAdsPagingAdapter.kt
+++ app/src/main/java/kg/o/internlabs/omarket/presentation/ui/fragments/detailAd/adapter/SimilarAdsPagingAdapter.kt
@@ -117,8 +117,11 @@ class SimilarAdsPagingAdapter : PagingDataAdapter<ResultX, SimilarAdsPagingAdapt
                 oldPriceProduct.visibility = View.GONE
             } else {
                 oldPriceProduct.visibility = View.VISIBLE
-                if (item.oldPrice == "10000 с") oldPriceProduct.text = item.oldPrice
-                else oldPriceProduct.text = item.oldPrice.toInt().formatDecimalSeparator()
+                try {
+                    oldPriceProduct.text = item.oldPrice.toInt().formatDecimalSeparator()
+                } catch (e: Exception) {
+                    oldPriceProduct.text = item.oldPrice
+                }
                 oldPriceProduct.paintFlags =
                     oldPriceProduct.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
             }
